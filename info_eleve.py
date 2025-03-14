794f1bc9a6011dd023c0474c2d137157f7c235c2
RICHY Takam <richytakam016@gmail.com>
2025-03-14 21:33:44 +0100

    ajout de fonction info eleve

---
+++ info_eleve.py
@@ -0,0 +1,13 @@
+def obtenir_informations_eleve():
+    # Création d'un dictionnaire avec les informations d'un élève
+    eleve = {
+        "nom": input("Entrez le nom de l'élève : "),
+        "age": int(input("Entrez l'âge de l'élève : ")),
+        "classe": input("Entrez la classe de l'élève : ")
+    }
+    return eleve
+
+# Exemple d'utilisation
+eleve_infos = obtenir_informations_eleve()
+print("\nInformations sur l'élève :")
+
